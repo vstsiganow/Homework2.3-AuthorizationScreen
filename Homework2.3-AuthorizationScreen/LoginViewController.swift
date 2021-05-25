a91887deaa4457de02bdc9d6470772de7b9ca739
vstsiganow <slavok-48@mail.ru>
2021-05-25 09:10:44 +0300

    Initial Commit

---
+++ Homework2.3-AuthorizationScreen/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.3-AuthorizationScreen
+//
+//  Created by vtsyganov on 24.05.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
