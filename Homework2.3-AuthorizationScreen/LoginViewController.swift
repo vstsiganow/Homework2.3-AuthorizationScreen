13c4dc6ca96cd29523dbd57971e221c475fee9fa
vstsiganow <slavok-48@mail.ru>
2021-05-25 09:33:31 +0300

    added TextFields

--- Homework2.3-AuthorizationScreen/LoginViewController.swift
+++ Homework2.3-AuthorizationScreen/LoginViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginTextField.
+        
+        
     }
 
 
